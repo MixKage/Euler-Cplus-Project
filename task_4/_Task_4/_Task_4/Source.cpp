bac2ae0017aa56cd5584c9462f5d2b5c851b36ff
MixKage <shishov265@gmail.com>
2021-01-07 00:49:41 +0300

    add_new_task

---
+++ task_4/_Task_4/_Task_4/Source.cpp
@@ -0,0 +1,44 @@
+//Ќайдите самый большой палиндром, состо€щий из произведени€ двух трехзначных чисел.
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool _isPol(int *temp)
+{
+	string str{};
+	str = to_string(*temp);
+	int len = str.length();
+	for (int i = 0; i < len / 2; ++i)
+	{
+		if (str[i] != str[len - i - 1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int num_1 = 0;
+	int num_2 = 0;
+	int temp = 0;
+	int sum = 0;
+
+	for(int i=100;i<1000;i++)
+	{
+		for(int l=100;l<1000;l++)
+		{
+			temp = i * l;
+			if(temp>sum)
+			{
+				if(_isPol(&temp))
+				{
+					sum = temp;
+				}
+			}
+		}
+	}
+	cout << sum;
+	return 0;
+}
